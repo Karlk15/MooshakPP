9c97dfc14e365eee996fae76ffae33a3ef0053cd
Kristofer Andersen <kristofer15@ru.is>
2016-05-02 17:57:54 +0000

    Adding empty project

---
+++ Mooshak++/Mooshak++/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mooshak__.Startup))]
+namespace Mooshak__
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
