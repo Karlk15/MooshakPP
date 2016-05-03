1aab5f9269a96469cde50087c3dadccc5ecf9025
Kristofer Andersen <kristofer15@ru.is>
2016-05-03 18:27:05 +0000

    Bjó til entities

---
+++ MooshakPP/Models/Entities/Assignment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MooshakPP.Models.Entities
+{
+    public class Assignment
+    {
+        public int ID { get; set; }
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+    }
+}
