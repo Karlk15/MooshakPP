755878add9934f054ef4100c80d2a6fa62564786
Karl Kristjánsson <karlk15@ru.is>
2016-05-04 12:52:17 +0000

    ViewModels added:
    +AddConnectionsViewModel
    +CreateCourseViewModels

---
+++ MooshakPP/Models/ViewModels/AddConnectionsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MooshakPP.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MooshakPP.Models.ViewModels
+{
+    public class AddConnectionsViewModel
+    {
+        public List<Course> courses{ get; set; }
+        public List<User> connectedUser { get; set; }
+        public List<User> notConnectedUser { get; set; }
+    }
+}
