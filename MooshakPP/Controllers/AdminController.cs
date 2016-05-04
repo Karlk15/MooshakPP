5c4ad61f2b9c6deb25aa166d5aaa043a277ae73e
Arnar Sævarsson <arnarfs15@ru.is>
2016-05-04 12:49:48 +0000

    Admin controller setup

---
+++ MooshakPP/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MooshakPP.Controllers
+{
+    public class AdminController : BaseController
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateCourse()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCourse(FormCollection collection)
+        {
+            return View();
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(FormCollection collection)
+        {
+            return View();
+        }
+
+        public ActionResult ConnectUser(int courseID)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ConnectUser(FormCollection collection)
+        {
+            return View();
+        }
+    }
+}
