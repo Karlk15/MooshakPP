c4a4ad8ff6e75cf9bffbffe5bc624401db0aa72f
Arnar Sævarsson <arnarfs15@ru.is>
2016-05-05 22:12:22 +0000

    base for student service added + teacherservice inherits from student service

--- MooshakPP/Services/StudentService.cs
+++ MooshakPP/Services/StudentService.cs
@@ -1,4 +1,5 @@
-﻿using MooshakPP.Models;
+﻿using MooshakPP.Models.ViewModels;
+using MooshakPP.Models.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,66 @@ namespace MooshakPP.Services
         {
             return null;
         }
+
+        public SubmissionViewModel Submissions(int userId, int courseId, int assignmentId, int milestoneId)
+        {
+            return null;
+        }
+
+        public DescriptionViewModel Description(int userId, int courseId, int assignmentId, int milestoneId)
+        {
+            return null;
+        }
+
+        public DetailsViewModel Details(int userId, int courseId, int assignmentId, int milestoneId, int submissionId)
+        {
+            return null;
+        } 
+
+        public bool CreateSubmission(Submission studentSubmission)
+        {
+            return true;
+        }
+
+        protected List<Course> GetCourses(int userId)
+        {
+            return null;
+        }
+
+        protected Course GetCourseByID(int courseId)
+        {
+            return null;
+        }
+
+        protected List<Assignment> GetAssignments(int courseId)
+        {
+            return null;
+        }
+
+        protected Assignment GetAssignmentByID(int assignmentId)
+        {
+            return null;
+        }
+
+        protected List<Milestone> GetMilestones(int assignmentId)
+        {
+            return null;
+        }
+
+        protected Milestone GetMilestoneByID(int milestoneId)
+        {
+            return null;
+        }
+
+        protected List<Submission> GetSubmissions(int userId, int milestoneId)
+        {
+            return null;
+        }
+
+        protected Submission GetSubmissionByID(int submissionId)
+        {
+            return null;
+        }
+
     }
 }
