2a3089b952c6b918c6078d39cae9262651363535
HrafnkellNK <hrafnkellnk15@ru.is>
2016-05-05 22:30:30 +0000

    Finishing of the add assignment functionality in teacher controller and service

--- MooshakPP/Controllers/TeacherController.cs
+++ MooshakPP/Controllers/TeacherController.cs
@@ -27,6 +27,8 @@ namespace MooshakPP.Controllers
             //if (id.HasValue)
             //{
             //int courseID = id.Value;
+
+            //all uncommented code in this ActionResult is temporary
             int courseID = 1;
             CreateAssignmentViewModel model = service.AddAssignment(courseID);
             return View(model);
@@ -46,15 +48,18 @@ namespace MooshakPP.Controllers
                 string tempCourseID = collection["courseID"];
                 model.courseID = Int32.Parse(tempCourseID);
 
-                allAssignments = service.AddAssignment(model.courseID);
+                model.title = collection["newAssignment.title"];
 
-                model.title = collection["title"];
-
-                string tempDueDate = collection["dueDate"];
+                //adding a default time to the due date of the assignment
+                string tempDueDate = collection["newAssignment.dueDate"];
                 tempDueDate = tempDueDate + " 23:59:59.42";
                 model.dueDate = Convert.ToDateTime(tempDueDate);
 
+                //adding the new assignment to the database through the TeacherService
                 service.CreateAssignment(model);
+
+                //getting the new list of assignments with the new assignment added ton the database
+                allAssignments = service.AddAssignment(model.courseID);
 
                 RedirectToAction("Create", allAssignments);
             }
