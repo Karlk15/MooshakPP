3c67fd9033e8a052813966f108de584871c2a5c1
valdimar94 <valdimarj14@ru.is>
2016-05-05 13:05:04 +0000

    Added AllSubmissionsViewModel and CreateMilestoneViewModel to ViewModels

---
+++ MooshakPP/Models/ViewModels/CreateMilestoneViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MooshakPP.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MooshakPP.Models.ViewModels
+{
+    public class CreateMilestoneViewModel
+    {
+        public List<Milestone> milestones { get; set; }
+    }
+}
