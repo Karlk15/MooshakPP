c5ef5bf22292c203b7018412ddf3bec8c96fabba
Arnar Sævarsson <arnarfs15@ru.is>
2016-05-05 21:19:05 +0000

    little student service add

---
+++ MooshakPP/Services/StudentService.cs
@@ -0,0 +1,16 @@
+﻿using MooshakPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MooshakPP.Services
+{
+    public class StudentService
+    {
+        public IndexViewModel Index(int userId, int courseId, int assignmentId)
+        {
+            return null;
+        }
+    }
+}
