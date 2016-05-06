5eb4bb9dd55907ed5cbfc18a7feaab809cf5a06c
HrafnkellNK <hrafnkellnk15@ru.is>
2016-05-06 22:55:57 +0000

    Created the relationship entity for users and courses

---
+++ MooshakPP/Models/Entities/UsersInCourse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MooshakPP.Models.Entities
+{
+    public class UsersInCourse
+    {
+        public int userID { get; set; }
+        public int courseID { get; set; }
+    }
+}
