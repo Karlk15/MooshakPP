9b1e8df41cde097a707fc4cbdcf2f93ed8ae86c7
valdimar94 <valdimarj14@ru.is>
2016-05-06 17:07:00 +0000

    made small changes

--- MooshakPP/Services/TeacherService.cs
+++ MooshakPP/Services/TeacherService.cs
@@ -8,7 +8,7 @@ using System.Web;
 
 namespace MooshakPP.Services
 {
-    public class TeacherService : StudentService
+    public class TeacherService
     {
         private ApplicationDbContext db;
 
@@ -17,40 +17,7 @@ namespace MooshakPP.Services
             db = new ApplicationDbContext();
         }
 
-        /// <summary>
-        /// NEEDS TO BE MOVED.
-        /// This function should be a protected function in "StudentService" which this class inherites from.
-        /// So basically a temporary function for testing.
-        /// </summary>
-        protected List<Assignment> GetAllAssignments(int courseId)
-        {
-        
-            var assignments = (from a in db.Assignments
-                               where a.courseID == courseId
-                               select a).ToList();
 
-            return assignments;
-        }
-
-        public CreateAssignmentViewModel AddAssignment(int courseId)
-        {
-            CreateAssignmentViewModel allAssignments = new CreateAssignmentViewModel();
-
-            allAssignments.assignments = new List<Assignment>(GetAllAssignments(courseId));
-
-            //created a single assignment for the Post request in the Teacher controller
-            //so now we have a courseID for our new assignment
-            allAssignments.newAssignment = new Assignment();
-            allAssignments.newAssignment.courseID = courseId;
-
-            return allAssignments;
-        }
-        
-        public void CreateAssignment(Assignment newAssignment)
-        {
-            db.Assignments.Add(newAssignment);
-            db.SaveChanges();
-        }
 
     }
 }
