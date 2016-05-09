fcfa53ad4305992c898827db5a68efbbace292cf
Kristofer Andersen <kristofer15@ru.is>
2016-05-09 10:20:39 +0000

    byrja á submit code placeholder

--- MooshakPP/Controllers/StudentController.cs
+++ MooshakPP/Controllers/StudentController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
+
 
 namespace MooshakPP.Controllers
 {
     [Authorize(Roles = "student")]
     public class StudentController : BaseController
     {
-        // GET: Student
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
@@ -18,7 +20,48 @@ namespace MooshakPP.Controllers
         [HttpPost]
         public ActionResult Submit(FormCollection collection)
         {
-            return View();
+            //placeholder code
+            var code = "#include <iostream>\n" +
+                    "using namespace std;\n" +
+                    "int main()\n" +
+                    "{\n" +
+                    "cout << \"Hello world\" << endl;\n" +
+                    "cout << \"The output should contain two lines\" << endl;\n" +
+                    "return 0;\n" +
+                    "}";
+
+            // Set up our working folder, and the file names/paths.
+            // In this example, this is all hardcoded, but in a
+            // real life scenario, there should probably be individual
+            // folders for each user/assignment/milestone.
+            var workingFolder = "C:\\Temp\\Mooshak2Code\\";
+            var cppFileName = "Hello.cpp";
+            var exeFilePath = workingFolder + "Hello.exe";
+
+            // Write the code to a file, such that the compiler
+            // can find it:
+            System.IO.File.WriteAllText(workingFolder + cppFileName, code);
+
+            // In this case, we use the C++ compiler (cl.exe) which ships
+            // with Visual Studio. It is located in this folder:
+            var compilerFolder = "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\bin\\";
+
+
+            Process compiler = new Process();
+            compiler.StartInfo.FileName = "cmd.exe";
+            compiler.StartInfo.WorkingDirectory = workingFolder;
+            compiler.StartInfo.RedirectStandardInput = true;
+            compiler.StartInfo.RedirectStandardOutput = true;
+            compiler.StartInfo.UseShellExecute = false;
+
+            compiler.Start();
+            compiler.StandardInput.WriteLine("\"" + compilerFolder + "vcvars32.bat" + "\"");
+            compiler.StandardInput.WriteLine("cl.exe /nologo /EHsc " + cppFileName);
+            compiler.StandardInput.WriteLine("exit");
+            string output = compiler.StandardOutput.ReadToEnd();
+            compiler.WaitForExit();
+            compiler.Close();
+            return RedirectToAction("Index");
         }
 
         [HttpGet]
