a3a25db162d260a891d73844973eac12cb22e2ea
valdimar94 <valdimarj14@ru.is>
2016-05-11 16:22:47 +0000

    Added validation for teacher create assignment

--- MooshakPP/Controllers/TeacherController.cs
+++ MooshakPP/Controllers/TeacherController.cs
@@ -64,78 +64,115 @@ namespace MooshakPP.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(CreateAssignmentViewModel collection, int? courseID,  int? assignmentID, string action)
+        public ActionResult Create(CreateAssignmentViewModel collection, int? courseID, int? assignmentID, string action)
         {
             Assignment model = new Assignment();
+            bool hasErrors = false;
 
-            if (ModelState.IsValid)
+            if (action == "delete")
+            {
+                if (assignmentID != null)
+                {
+                    service.RemoveAssignment((int)assignmentID);
+                }
+
+                return RedirectToAction("Create");
+
+            }
+            else if (action == "create")
             {
 
-                if (action == "delete")
+                if (collection.currentAssignment.title == "" || collection.currentAssignment.title == null)
                 {
-                    if (assignmentID != null)
+                    hasErrors = true;
+                    ModelState.AddModelError("currentAssignment.title", "You must give a title");
+                }
+
+                if (collection.start == "" || collection.start == null)
+                {
+                    hasErrors = true;
+                    ModelState.AddModelError("start", "You must give a start date");
+                }
+
+                if (collection.due == "" || collection.due == null)
+                {
+                    hasErrors = true;
+                    ModelState.AddModelError("due", "You must give a due date");
+                }
+
+                if (hasErrors == true)
+                {
+                    if (courseID == null)
                     {
-                        service.RemoveAssignment((int)assignmentID);
+                        courseID = service.GetFirstCourse(User.Identity.GetUserId());
                     }
 
-                    return RedirectToAction("Create");
-
+                    CreateAssignmentViewModel emptyModel = service.AddAssignment(User.Identity.GetUserId(), (int)courseID, assignmentID);
+                    
+                    Assignment noAssignment = new Assignment();
+                    noAssignment.title = "";
+                    noAssignment.ID = 0;
+                    noAssignment.courseID = (int)courseID;
+                    emptyModel.currentAssignment = noAssignment;
+                    emptyModel.currentCourse.ID = (int)courseID;
+                    emptyModel.currentAssignment.ID = (int)assignmentID;
+                    
+                    return View(emptyModel);
                 }
-                else if (action == "create")
+
+                model.courseID = (int)courseID;
+
+                model.title = collection.currentAssignment.title;
+
+                //adding a default time to the start date of the assignment and parsing the right format to avoid errors
+                string tempStartDate = collection.start;
+                tempStartDate = tempStartDate + " 00:01:00";
+                model.startDate = DateTime.ParseExact(tempStartDate, "MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+
+                //adding a default time to the due date of the assignment and parsing the right format to avoid errors
+                string tempDueDate = collection.due;
+                tempDueDate = tempDueDate + " 23:59:59";
+                model.dueDate = DateTime.ParseExact(tempDueDate, "MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+
+                model.teacherID = User.Identity.GetUserId();
+
+                //adding the new assignment to the database through the TeacherService
+                service.CreateAssignment(model);
+
+                    
+                return RedirectToAction("Create", new { courseid = courseID, assignmentid = model.ID});
+            }
+            else if(action == "edit")
+            {
+                if(assignmentID != null)
                 {
                     model.courseID = (int)courseID;
-
+                    model.ID = (int)assignmentID;
                     model.title = collection.currentAssignment.title;
-
-                    //adding a default time to the due date of the assignment and parsing the right format to avoid errors
+                    //adding a default time to the start date of the assignment and parsing the right format to avoid errors
                     string tempDueDate = collection.due;
                     tempDueDate = tempDueDate + " 23:59:59";
-                    model.dueDate = DateTime.ParseExact(tempDueDate, "MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                    //model.dueDate = DateTime.ParseExact(tempDueDate, "MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                    model.dueDate = Convert.ToDateTime(tempDueDate);
 
                     //adding a default time to the start date of the assignment and parsing the right format to avoid errors
                     string tempStartDate = collection.start;
                     tempStartDate = tempStartDate + " 00:01:00";
-                    model.startDate = DateTime.ParseExact(tempStartDate, "MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
-                    model.teacherID = User.Identity.GetUserId();
+                    //model.startDate = DateTime.ParseExact(tempStartDate, "MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                    model.startDate = Convert.ToDateTime(tempStartDate);
 
-                    //adding the new assignment to the database through the TeacherService
-                    service.CreateAssignment(model);
+                    service.EditAssignment(model);
 
-                    
-                    return RedirectToAction("Create", new { courseid = courseID, assignmentid = model.ID});
-                }
-                else if(action == "edit")
-                {
-                    if(assignmentID != null)
-                    {
-                        model.courseID = (int)courseID;
-                        model.ID = (int)assignmentID;
-                        model.title = collection.currentAssignment.title;
-                        //adding a default time to the start date of the assignment and parsing the right format to avoid errors
-                        string tempDueDate = collection.due;
-                        tempDueDate = tempDueDate + " 23:59:59";
-                        //model.dueDate = DateTime.ParseExact(tempDueDate, "MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
-                        model.dueDate = Convert.ToDateTime(tempDueDate);
-
-                        //adding a default time to the start date of the assignment and parsing the right format to avoid errors
-                        string tempStartDate = collection.start;
-                        tempStartDate = tempStartDate + " 00:01:00";
-                        //model.startDate = DateTime.ParseExact(tempStartDate, "MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
-                        model.startDate = Convert.ToDateTime(tempStartDate);
-
-                        service.EditAssignment(model);
-
-                        return RedirectToAction("Create", new { courseid = courseID, assignmentid = assignmentID });
-                    }
-                }
-                else if(action == "recover")
-                {
-                    service.RecoverAssignment((int)courseID, (int)assignmentID);
                     return RedirectToAction("Create", new { courseid = courseID, assignmentid = assignmentID });
                 }
             }
-
+            else if(action == "recover")
+            {
+                service.RecoverAssignment((int)courseID, (int)assignmentID);
+                return RedirectToAction("Create", new { courseid = courseID, assignmentid = assignmentID });
+            }
             return View("Error");
+
         }
 
         [HttpGet]
