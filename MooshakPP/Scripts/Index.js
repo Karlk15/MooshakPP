bbb43e1bbcee5b244501ff6f6e0d9c851e17325b
Karl Kristjánsson <karlk15@ru.is>
2016-05-11 10:05:43 +0000

    no changes because Vladimir was so clever

---
+++ MooshakPP/Scripts/Index.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function(){
+    $("#tabs").tabs();
+   
+    $(".btnNext").click(function () {
+       
+        $("#tabs").tabs("option", "active", $("#tabs").tabs('option', 'active') + 2);
+    });
+});
