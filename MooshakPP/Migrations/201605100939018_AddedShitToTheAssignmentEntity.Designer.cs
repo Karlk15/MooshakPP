f8f2a5a566897fe1cce42829c71f9fc61b021843
HrafnkellNK <hrafnkellnk15@ru.is>
2016-05-16 15:18:23 +0000

    Got rid of trash class diagrams

--- MooshakPP/Migrations/201605100939018_AddedShitToTheAssignmentEntity.Designer.cs
+++ MooshakPP/Migrations/201605100939018_AddedShitToTheAssignmentEntity.Designer.cs
@@ -5,25 +5,4 @@ namespace MooshakPP.Migrations
     using System.Data.Entity.Migrations;
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
-    
-    [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class AddedShitToTheAssignmentEntity : IMigrationMetadata
-    {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(AddedShitToTheAssignmentEntity));
-        
-        string IMigrationMetadata.Id
-        {
-            get { return "201605100939018_AddedShitToTheAssignmentEntity"; }
-        }
-        
-        string IMigrationMetadata.Source
-        {
-            get { return null; }
-        }
-        
-        string IMigrationMetadata.Target
-        {
-            get { return Resources.GetString("Target"); }
-        }
-    }
 }
--- MooshakPP/Migrations/201605100939018_AddedShitToTheAssignmentEntity.cs
+++ MooshakPP/Migrations/201605100939018_AddedShitToTheAssignmentEntity.cs
@@ -1,19 +1,4 @@
 namespace MooshakPP.Migrations
 {
     using System.Data.Entity.Migrations;
-
-    public partial class AddedShitToTheAssignmentEntity : DbMigration
-    {
-        public override void Up()
-        {
-            AddColumn("dbo.Assignments", "isDeleted", c => c.Boolean(nullable: false));
-            AddColumn("dbo.Assignments", "teacherID", c => c.String());
-        }
-        
-        public override void Down()
-        {
-            DropColumn("dbo.Assignments", "teacherID");
-            DropColumn("dbo.Assignments", "isDeleted");
-        }
-    }
 }
